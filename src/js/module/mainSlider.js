8c078989bb41c3124e739d767abb3a4b15bd3101
genexys <magicilusha@gmail.com>
2021-01-15 18:47:23 +0300

    edit slider, add section about services, add products card and list

--- src/js/module/mainSlider.js
+++ src/js/module/mainSlider.js
@@ -1,9 +1,21 @@
-import Swiper from 'swiper';
-// import 'swiper/swiper-bundle.css';
+import Swiper, { Navigation } from 'swiper';
+Swiper.use([Navigation]);
 
 const mainSlider = () => {
-  var mySwiper = new Swiper('.main-slider__container', {
+  const mainSlider = new Swiper('.main-slider__container', {
+    autoHeight: true,
+    calculateHeight:true,
+    autoplay: {
+      delay: 5000,
+    },
     loop: true,
+    observer: true,
+    observeParents: true,
+
+    navigation: {
+      nextEl: '.main-slider__button--next',
+      prevEl: '.main-slider__button--prev',
+    },
   })
 }
 
